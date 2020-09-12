84139f5e36e0bffb4e129c2d43599df523e7763b
SayaniG371 <55864408+SayaniG371@users.noreply.github.com>
2020-09-13 00:13:21 +0530

    Update PyMaster.py

--- PyMaster.py
+++ PyMaster.py
@@ -6,17 +6,20 @@ import contextlib
 import sqlite3
 from tkinter import messagebox
 
+# Configuration of the main tkinter window 
 root = Tk()
 root.title("PyMaster")
 root.iconbitmap('disneyland.ico')
 root.geometry("2000x2000")
 
+# Setting the Background image in main window
 filename = PhotoImage(file="poster.png")
 background_label = Label(root, image=filename)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-
+# Enter function : called from main window to check authorization locally
 def Enter(id, pwd):
+    # Connect to the database
     conn = sqlite3.connect('data_book.db')
 
     # Create cursor
@@ -27,20 +30,27 @@ def Enter(id, pwd):
     recs = c.fetchall()
     flag = 0
 
+    # Reading records from table and checking authorization
     for rec in recs:
         if (pwd == rec[1]) and (id == rec[0]):
                 flag = 1
                 break
+    # If credentials are valid then open new window
     if flag == 1:
+        
+        # New window configuration
         et = Toplevel()
         et.title("PyMaster")
         et.iconbitmap('disneyland.ico')
         et.configure(bg="Pink")
         et.geometry("2000x2000")
 
-
-        def sel():
+        # Select Function : called from next window to select the topic to learn 
+        def Select():
+            # Recieving option from user
             res = character.get()
+            
+            # 1st Choice of Radio Button
             if int(res) == 1:
                 global i
                 global s2
@@ -50,9 +60,12 @@ def Enter(id, pwd):
                 global btn3
                 global btn4
                 i = 0
+                
+                # Labels 
                 lblm = Label(et, background="skyblue", padx=10, pady=10, text="Strings in Python3 ",
                              font=("Helvetica", 30))
                 lblm.place(x=950, y=100, anchor="center")
+                
                 s1 = Label(et, background="skyblue", width=300, height=50,
                            text="In Python, Strings are arrays of bytes representing" +
                                 " Unicode characters. However, Python does not have a " +
@@ -64,27 +77,44 @@ def Enter(id, pwd):
                                 "\nRun the Follwing Codes one by one !!",
                            font=("Helvetica", 15), wraplength=1100)
                 s1.place(x=670, y=270, anchor="center", relheight=0.2)
+                
+                # Run Function to run the code and get the output 
                 def Run(code):
                     st = ""
                     @contextlib.contextmanager
                     def stdoutIO(stdout=None):
+                        # Passing the current output of system to variable 'old'
                         old = sys.stdout
+                        
+                        # If nothing is passed as new outpt then an empty string is created as new output
                         if stdout is None:
                             stdout = StringIO()
+                        # The new output is stored
                         sys.stdout = stdout
+                        # Yielding the new output as string
                         yield stdout
+                        # Returning the original ouput to system
                         sys.stdout = old
-
+                        
+                    # Using the stdIO function to produce output as string
                     with stdoutIO() as s:
                         try:
+                            # Passing the output to s, if runs successfully returns output 
                             exec(code)
+                            
                         except:
-                            print("Something wrong with the code")
+                            # For exception handling returns message Check the code
+                            print("Check the code!!")
+                            
+                    # Storing yielded value of output in 
                     st = s.getvalue()
+
+                    # Label for final output
                     global lbl
                     lbl = Label(et, text="Output : \n" + st, font=("Helvetica", 10), wraplength=500)
                     lbl.place(x=950, y=500, anchor="center")
-
+                
+                # Refresh1 Function : called from new window after topic selection, to refresh the whole window     
                 def Ref1():
                     s2.destroy()
                     txt.destroy()
@@ -93,17 +123,22 @@ def Enter(id, pwd):
                     btn3.destroy()
                     btn4.destroy()
                     btn5.destroy()
-
+                    
+                # Refresh2 Function : called from new window to refresh the output of code
                 def Ref2():
                     lbl.destroy()
 
+                # Backtohome Function : called from new window to return to main window
                 def Backtohm():
                     et.destroy()
 
+                # Change Function : called from new window to traverse through the various example codes with index number
                 def Change(i):
                     Ref1()
-
+                    
+                    # When reaching the first example code
                     if i == 0:
+                        # Labels and Buttons
                         s2 = Label(et, padx=10, pady=10, text="Creating a String with single Quotes  :Code "+str(i+1)+" of 3",
                                    font=("Helvetica", 20))
                         s2.place(x=650, y=370, anchor="center")
@@ -134,8 +169,10 @@ def Enter(id, pwd):
                         btn5 = Button(et, text="BACK", padx=15, pady=10, state=DISABLED)
                         btn5.configure(font=("Helvetica", 10))
                         btn5.place(x=750, y=600, anchor="center")
-
+                        
+                    # When reaching the second example code
                     if i == 1:
+                        # Labels and Buttons
                         s2 = Label(et, padx=10, pady=10, text="Creating a String with double Quotes  :Code "+str(i+1)+" of 3",
                                    font=("Helvetica", 20))
                         s2.place(x=650, y=370, anchor="center")
@@ -166,8 +203,10 @@ def Enter(id, pwd):
                         btn5 = Button(et, text="BACK", padx=15, pady=10, command=lambda: Change(i - 1))
                         btn5.configure(font=("Helvetica", 10))
                         btn5.place(x=750, y=600, anchor="center")
-
+                        
+                    # When reaching the third example code
                     if i == 2:
+                        # Labels and Buttons
                         s2 = Label(et, padx=10, pady=10, text="Creating a String with triple Quotes  :Code "+str(i+1)+" of 3",
                                    font=("Helvetica", 20))
                         s2.place(x=650, y=370, anchor="center")
@@ -199,6 +238,9 @@ def Enter(id, pwd):
                         btn5.configure(font=("Helvetica", 10))
                         btn5.place(x=750, y=600, anchor="center")
 
+                  
+                # The very first view after selecting topic , i.e, 1st example code
+                # Labels and Buttons
                 s2 = Label(et, padx=10, pady=10, text="Creating a String with single Quotes  : Code "+str(i+1)+" of 3",
                            font=("Helvetica", 20))
                 s2.place(x=650, y=370, anchor="center")
@@ -230,34 +272,43 @@ def Enter(id, pwd):
                 btn5.configure(font=("Helvetica", 10))
                 btn5.place(x=750, y=600, anchor="center")
 
+        
+        # The very first view in new window to select topic for learning
         labl1 = Label(et, text="Choose topic : ", padx=20, pady=20)
         labl1.configure(font=("Helvetica", 30, "bold italic"))
         labl1.place(x=200, y=100, anchor="center")
 
-
+        # Defining the topics
         MODES = [
             ("Strings", "1"),
         ]
 
+        # Creating a Stringvar to store selected option
         character = StringVar()
         character.set("1")
         x, y = 400, 50
+        # Loop to put radiobuttons on screen
         for text, mode in MODES:
             rd = Radiobutton(et, text=text, variable=character, value=mode)
             rd.configure(font=("Helvetica", 20, "bold italic"))
             rd.place(x=x, y=y)
             y += 50
 
-        btn = Button(et, text="Select", width=15, command=sel)
+        # Button to select
+        btn = Button(et, text="Select", width=15, command=Select)
         btn.configure(font=("Helvetica", 20, "bold italic"))
         btn.place(x=400, y=120)
 
         et.mainloop()
 
+    # In case of invalid credentials popup warning message
     else:
+        # defining warning popup window with title "Invalid!!", and message = "Invalid Credentials" 
         response = messagebox.showwarning("Invalid!!", "Invalid Credentials")
 
+# User Function : called from main window to update users in database
 def user():
+    # Configuring the manage user window
     root2 = Tk()
     root2.title("Users")
     root2.iconbitmap("disneyland.ico")
@@ -265,21 +316,23 @@ def user():
 
     # DataBases
 
-    # Create a database or connect to one
+    # Connect to database
     conn = sqlite3.connect('data_book.db')
 
     # Create cursor
     c = conn.cursor()
 
+    # Edit Function : Called from the update function on manage user window to edit information of existing users
     def edit():
-        # Create a database or connect to one
+        # Connect to databas
         conn = sqlite3.connect('data_book.db')
 
         # Create cursor
         c = conn.cursor()
 
         rid = del_box.get()
-
+        
+        # Updating information in table
         c.execute("""UPDATE addresses SET
 
             idt = :id,
@@ -300,6 +353,7 @@ def user():
         conn.close()
         ed.destroy()
 
+    # Update Function : Called from the manage user window to edit information of existing users
     def update():
         global ed
         ed = Tk()
@@ -307,14 +361,14 @@ def user():
         ed.iconbitmap("disneyland.ico")
         ed.geometry("500x500")
 
-        # Create a database or connect to one
+        # Connect to database
         conn = sqlite3.connect('data_book.db')
 
         # Create cursor
         c = conn.cursor()
 
         rid = del_box.get()
-        # Insert into table
+        # Fetching information from table
         c.execute("SELECT* FROM addresses WHERE oid=" + rid)
         recs = c.fetchall()
 
@@ -347,7 +401,8 @@ def user():
         # Create Save Btn
         s_btn = Button(ed, text="Save", command=edit)
         s_btn.grid(row=11, column=0, columnspan=2, padx=10, pady=10, ipadx=137)
-
+   
+    # Delete Function : Called from the manage user window to delete information of existing users
     def delete():
         # Create a database or connect to one
         conn = sqlite3.connect('data_book.db')
@@ -355,6 +410,7 @@ def user():
         # Create cursor
         c = conn.cursor()
 
+        # Deleting information from table
         c.execute("DELETE from addresses WHERE oid=" + del_box.get())
         del_box.delete(0, END)
 
@@ -364,6 +420,7 @@ def user():
         # Close Connection
         conn.close()
 
+    # Submit Function : Called from the manage user window to add information of new users
     def Submit():
         # Create a database or connect to one
         conn = sqlite3.connect('data_book.db')
@@ -386,16 +443,17 @@ def user():
 
         # clear TextBoxes
         id.delete(0, END)
-        pwd.delete(0, END)
-
+        pwd.delete(0, END) 
+    
+    # Show Function : Called from the manage user window to show the id and Sr no. of existing users
     def show():
-        # Create a database or connect to one
+        # Connect to database
         conn = sqlite3.connect('data_book.db')
 
         # Create cursor
         c = conn.cursor()
 
-        # Insert into table
+        # Fetching information from table
         c.execute("SELECT*, oid FROM addresses")
         recs = c.fetchall()
         # print(recs)
@@ -414,6 +472,8 @@ def user():
         # Close Connection
         conn.close()
 
+    
+    # View Configuration of Manage user window
     # Create Text Boxes
     id = Entry(root2, width=30)
     id.grid(row=0, column=1, padx=20)
@@ -459,6 +519,7 @@ def user():
     conn.close()
 
 
+# View Configuration of main window
 # Welcome msg
 l1 = Label(root, padx=30, text="Welcome  To  PyMaster !", font=("Helvetica", 50))
 l1.place(x=800, y=100, anchor="center")
